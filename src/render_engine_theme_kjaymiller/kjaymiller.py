8385456b01b04cdcabaf38b43f47611dfbd1a478
Jay Miller <kjaymiller@gmail.com>
2023-07-23 20:06:08 -0700

    conform to new themeManager

--- src/render_engine_theme_kjaymiller/kjaymiller.py
+++ src/render_engine_theme_kjaymiller/kjaymiller.py
@@ -3,5 +3,7 @@ from jinja2 import PackageLoader
 from render_engine.utils.themes import Theme
 
 kjaymiller = Theme(
-    themeLoader = PackageLoader('render_engine_theme_kjaymiller', 'templates')
+    name="kjaymiller",
+    template_loader=PackageLoader("render_engine_theme_kjaymiller", "templates"),
+    static_loader=PackageLoader("render_engine_theme_kjaymiller", "static"),
 )
