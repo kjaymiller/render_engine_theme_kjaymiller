6542dffeaccf52359151a97bc2db91189e54bf17
Jay Miller <kjaymiller@gmail.com>
2023-07-23 20:07:44 -0700

    update new path

--- src/render_engine_theme_kjaymiller/kjaymiller.py
+++ src/render_engine_theme_kjaymiller/kjaymiller.py
@@ -3,7 +3,6 @@ from jinja2 import PackageLoader
 from render_engine.utils.themes import Theme
 
 kjaymiller = Theme(
-    name="kjaymiller",
-    template_loader=PackageLoader("render_engine_theme_kjaymiller", "templates"),
-    static_loader=PackageLoader("render_engine_theme_kjaymiller", "static"),
+    loader=PackageLoader("render_engine_theme_kjaymiller", "templates"),
+    static_dir= __file__.parent / "static",
 )
