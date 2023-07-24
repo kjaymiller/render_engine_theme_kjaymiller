48dfe59f1a2fe60dc82c1939537cdc254b34f1ee
Jay Miller <kjaymiller@gmail.com>
2023-07-23 20:08:57 -0700

    set pathlib

--- src/render_engine_theme_kjaymiller/kjaymiller.py
+++ src/render_engine_theme_kjaymiller/kjaymiller.py
@@ -1,8 +1,10 @@
 
+import pathlib
+
 from jinja2 import PackageLoader
 from render_engine.utils.themes import Theme
 
 kjaymiller = Theme(
     loader=PackageLoader("render_engine_theme_kjaymiller", "templates"),
-    static_dir= __file__.parent / "static",
+    static_dir= pathlib.Path(__file__).parent / "static",
 )
