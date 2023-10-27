5d048f7b7c653d5e868ea8adb62a214a1ff192bc
Jay Miller <kjaymiller@gmail.com>
2023-10-26 21:35:10 -0400

    rename globals to template globals

--- src/render_engine_theme_kjaymiller/kjaymiller.py
+++ src/render_engine_theme_kjaymiller/kjaymiller.py
@@ -38,5 +38,5 @@ kjaymiller = Theme(
         "theme_gradient_to": get_theme_gradient_to,
         "theme_color_value": get_color_string_value,
         },
-    globals = {"body_class": "dark:bg-slate-800 dark:text-slate-100"}
+    template_globals = {"body_class": "dark:bg-slate-800 dark:text-slate-100"}
 )
