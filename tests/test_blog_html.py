fb691b3136998bfd5abcc8c4f8d1d3ca7365972c
kjaymiller <kjaymiller@gmail.com>
2023-10-29 15:36:12 -0400

    adds blog test

---
+++ tests/test_blog_html.py
@@ -0,0 +1,8 @@
+import pytest
+
+
+def test_blog_title_class_is_title(site):
+    print(list(site.output_path.joinpath("blog").iterdir()))
+    assert site.output_path.joinpath("blog", "test-blog-post.html").read_text().find(
+        "class=\"title\""
+    )
