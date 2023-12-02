aa3a2c0f9609621e5faba9a3ddc65ddd1abc6f48
kjaymiller <kjaymiller@gmail.com>
2023-12-02 00:35:04 -0500

    updates versions

--- tests/test_blog_html.py
+++ tests/test_blog_html.py
@@ -1,5 +1,3 @@
-
-
 def test_blog_title_class_is_title(site):
     print(list(site.output_path.joinpath("blog").iterdir()))
     assert site.output_path.joinpath("blog", "test-blog-post.html").read_text().find('class="title"')
